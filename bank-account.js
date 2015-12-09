c0d23eb35a26a6cc6bbdda3a0e8be897e1cd4df9
Matthew Roche <mjroche@mail.usf.edu>
2015-12-09 13:43:27 -0500

    update bank acocunt

--- bank-account.js
+++ bank-account.js
@@ -1,6 +1,27 @@
+var accounts = [];
 var account = {
   balance: 0
 };
+
+
+
+//create account
+function createAccount (account) {
+  accounts.push(account);
+  return account;
+}
+
+function getAccount (username) {
+  var matchedAccount;
+
+  accounts.forEach(function (account) {
+    if (account.username === username) {
+      matchedAccount = account;
+    }
+  });
+  return matchedAccount;
+}
+
 //deposit
 function deposit (account, amount) {
   account.balance += amount;
@@ -19,11 +40,26 @@ function getBalance (account) {
 }
 
 
-//Tests
-deposit(account, 1000);
+var mattsAccount = createAccount({
+  username: 'matt',
+  balance: 0
+});
 
-console.log(getBalance(account));
+deposit(mattsAccount, 100);
+console.log(getBalance(mattsAccount));
 
-withdraw(account, 121);
+withdraw(mattsAccount, 11);
+console.log(getBalance(mattsAccount));
 
-console.log(getBalance(account));
+var existingAccount = getAccount('matt');
+console.log(getBalance(mattsAccount));
+
+var lizAccount = createAccount({
+  username: 'liz',
+  balance: 12
+})
+
+console.log(accounts)
+
+var existingLizAccount = getAccount('liz');
+console.log(existingLizAccount);
